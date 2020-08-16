b9eda1c1e54f562604ccee6af3085b1b1d311053
felipeRosas <feliperozam@gmail.com>
2020-08-16 18:57:54 -0400

    Respuesta pregunta1

--- pregunta1.js
+++ pregunta1.js
@@ -1,15 +1,44 @@
 
 /**
  * 
- * @param {number[]} integers 
+ * @param {number[]} integers
  */
 function findOutlier(integers) {
+
+    if( integers.length < 3 ){
+        return false;
+    }
+    //contar cantidad de pares e impares en array
+    let evenNumbers = 0;
+    let oddNumbers  = 0;
+    //guardar el indice del array en donde esta el Outlier
+    let evenIndex;
+    let oddIndex;
     
+    integers.forEach( (number, indice) => {
+
+        if(number%2 == 0){
+            evenNumbers++;
+            evenIndex = indice;
+            
+        } else {
+            oddNumbers++;
+            oddIndex = indice;
+        }
+
+    } );
+
+    if( evenNumbers > oddNumbers ){      
+        return integers[oddIndex];
+    } else {
+        return integers[evenIndex];
+    }
+
 }
 
 
-const test1 = [2, 4, 0, 100, 4, 11, 2602, 36]
-const test2 = [160, 3, 1719, 19, 11, 13, -21]
+const test1 = [2, 4, 0, 100, 4, 11, 2602, 36];
+const test2 = [160, 3, 1719, 19, 11, 13, -21];
 
-console.assert(findOutlier(test1) == 11)
-console.assert(findOutlier(test2) == 160)
+console.assert(findOutlier(test1) == 11);
+console.assert(findOutlier(test2) == 160);
